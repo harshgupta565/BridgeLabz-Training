ce513afc5aff7be16966af384f50f64596f98ae1
harshgupta565 <harsh.guptaengg675@gmail.com>
2025-12-23 11:48:40 +0530

    add.digitcounter[Harsh Gupta]

--- core-java-practice/Review/digitcounter.java
+++ core-java-practice/Review/digitcounter.java
@@ -1,15 +1,16 @@
 package Review;
+
 import java.util.Scanner;
-public class Review1 {
+public class digitcounter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         //int a = n;
         int count = 0;
         while ( n > 0) {
-           // int  digit  = a% 10;
             n = n / 10;
             count++;}
-            System.out.println(count);
-        }
+        System.out.println(count);
     }
+}
+
