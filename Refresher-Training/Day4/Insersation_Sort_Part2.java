37e1a09126adc2539daaab18ce705c1badd0a8bd
Harshgupta565 <harsh.guptaengg675@gmail.com>
2026-08-06 11:51:29 +0530

    [Harsh Gupta] add Insersation Sort Problem

---
+++ Refresher-Training/Day4/Insersation_Sort_Part2.java
@@ -0,0 +1,45 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class Insersation_Sort_Part2 {
+
+    public static void insertionSort2(int n, int[] arr) {
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+
+            for (int k = 0; k < n; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of array
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        insertionSort2(n, arr);
+
+        sc.close();
+    }
+}
