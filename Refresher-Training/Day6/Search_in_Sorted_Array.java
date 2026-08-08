65baa0428ebca77fc68ff52dc9b1185caf3517e4
Harshgupta565 <harsh.guptaengg675@gmail.com>
2026-08-08 23:49:00 +0530

    [Harsh Gupta] add Searching realated HackerRank Questions

---
+++ Refresher-Training/Day6/Search_in_Sorted_Array.java
@@ -0,0 +1,85 @@
+package Day6;
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Resultss {
+
+
+
+
+    public static List<Integer> circularArrayRotation(
+            List<Integer> a,
+            int k,
+            List<Integer> queries) {
+
+        int n = a.size();
+
+        // Avoid unnecessary rotations
+        k = k % n;
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int query : queries) {
+
+            int originalIndex = (query - k + n) % n;
+
+            result.add(a.get(originalIndex));
+        }
+
+        return result;
+    }
+}
+
+
+
+
+public class Search_in_Sorted_Array {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int q = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> queries = IntStream.range(0, q).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Resultss.circularArrayRotation(a, k, queries);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
