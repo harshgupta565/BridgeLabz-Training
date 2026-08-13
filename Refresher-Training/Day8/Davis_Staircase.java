2829e9753bbe3ea48b29340e924f6c3d5e10a2ac
Harshgupta565 <harsh.guptaengg675@gmail.com>
2026-08-13 10:30:52 +0530

    [Harsh Gupta] add Recursion  Solution

---
+++ Refresher-Training/Day8/Davis_Staircase.java
@@ -0,0 +1,74 @@
+package Day8;
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    static int[] dp;
+
+    public static int stepPerms(int n) {
+
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return solve(n);
+    }
+
+    public static int solve(int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 0;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = solve(n - 1)
+                + solve(n - 2)
+                + solve(n - 3);
+
+        return dp[n];
+    }
+}
+
+
+
+
+public class Davis_Staircase {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int s = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, s).forEach(sItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int res = Result.stepPerms(n);
+
+                bufferedWriter.write(String.valueOf(res));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
