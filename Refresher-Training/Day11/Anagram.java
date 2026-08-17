9e7daf819d1311ea5be5bf0bf035473cb03dfca3
Harshgupta565 <harsh.guptaengg675@gmail.com>
2026-08-17 10:28:07 +0530

    [Harsh Gupta] add String  Hackerrank Solution

---
+++ Refresher-Training/Day11/Anagram.java
@@ -0,0 +1,35 @@
+package Day11;
+import java.util.Scanner;
+
+public class Anagram {
+
+    static boolean isAnagram(String a, String b) {
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        char[] arr1 = a.toCharArray();
+        char[] arr2 = b.toCharArray();
+
+        java.util.Arrays.sort(arr1);
+        java.util.Arrays.sort(arr2);
+
+        return java.util.Arrays.equals(arr1, arr2);
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
